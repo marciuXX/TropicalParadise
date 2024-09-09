ea0488ec4769c7c8d37ffc75441f6ec18d055c6f
Michal <marc.michal01@gmail.com>
2024-09-09 19:07:56 +0200

    added js to handle nav

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 const nav = document.querySelector('.nav__box');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active');
+
+	navBtnBars.classList.remove('black-bars-color');
+
+	if(!nav.classList.contains('nav--active')){
+		handleObserver();
+	}
 
 	allNavItems.forEach((item) => {
 		item.addEventListener('click', () => {
@@ -24,4 +33,24 @@ const handleNavItemsAnimation = () => {
 	});
 };
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach((section) => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60 && !nav.classList.contains('nav--active')) {
+			navBtnBars.classList.add('black-bars-color');
+
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color');
+		}
+	});
+};
+
+handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver);
